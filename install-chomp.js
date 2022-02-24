dfe03ceddf52598e5d1061676dd5b0478b7b5334
Guy Bedford <guybedford@gmail.com>
2022-02-23 20:33:05 -0800

    fixup unix action

--- install-chomp.js
+++ install-chomp.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const { execSync } = require('child_process');
 const { readFile, writeFile } = require('fs/promises');
+const { mkdirSync } = require('fs');
 const { gunzipSync } = require('zlib');
 const path = require('path');
 
@@ -8,7 +9,7 @@ const path = require('path');
   const { default: fetch } = await import('node-fetch');
   const version = (core.getInput('version') || '').trim() || '0.1.8';
 
-  const os = (process.env.RUNNER_OS || 'windows').toLowerCase();
+  const os = (process.env.RUNNER_OS || 'linux').toLowerCase();
   if (os !== 'macos' && os !== 'windows' && os !== 'linux')
     throw new Error(`Unknown OS or unable to determine architecture (${process.env.RUNNER_OS})`);
 
@@ -30,7 +31,8 @@ const path = require('path');
   if (os === 'windows') {
     execSync(`powershell.exe "Expand-Archive chomp-${version}.zip"`);
   } else {
-    execSync(`tar -xz chomp-${version}.tar`);
+    mkdirSync(`chomp-${version}`);
+    execSync(`tar -xf chomp-${version}.tar -C chomp-${version}`);
   }
 
   const githubPath = await readFile(process.env.GITHUB_PATH, 'utf8');
