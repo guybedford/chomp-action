dddf5ff2595bc4e52a21bb3136ddff8c95e42fed
Guy Bedford <guybedford@gmail.com>
2022-02-23 20:34:28 -0800

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -1910,6 +1910,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(186);
 const { execSync } = __nccwpck_require__(81);
 const { readFile, writeFile } = __nccwpck_require__(292);
+const { mkdirSync } = __nccwpck_require__(147);
 const { gunzipSync } = __nccwpck_require__(796);
 const path = __nccwpck_require__(17);
 
@@ -1917,7 +1918,7 @@ const path = __nccwpck_require__(17);
   const { default: fetch } = await __nccwpck_require__.e(/* import() */ 841).then(__nccwpck_require__.bind(__nccwpck_require__, 841));
   const version = (core.getInput('version') || '').trim() || '0.1.8';
 
-  const os = (process.env.RUNNER_OS || 'windows').toLowerCase();
+  const os = (process.env.RUNNER_OS || 'linux').toLowerCase();
   if (os !== 'macos' && os !== 'windows' && os !== 'linux')
     throw new Error(`Unknown OS or unable to determine architecture (${process.env.RUNNER_OS})`);
 
@@ -1939,7 +1940,8 @@ const path = __nccwpck_require__(17);
   if (os === 'windows') {
     execSync(`powershell.exe "Expand-Archive chomp-${version}.zip"`);
   } else {
-    execSync(`tar -xz chomp-${version}.tar`);
+    mkdirSync(`chomp-${version}`);
+    execSync(`tar -xf chomp-${version}.tar -C chomp-${version}`);
   }
 
   const githubPath = await readFile(process.env.GITHUB_PATH, 'utf8');
